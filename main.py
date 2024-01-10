adbfbc6ba9e186997872770f4b7224817a590356
Daniel Gran <contact@grandaniel.com>
2024-01-10 12:17:27 +0100

    ugly-changes

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def main():
     print("bytes[2]: ", bytes[2])
     summ = (bytes[1] << 8) | bytes[2]
     print("Sum: " + str(summ))
-    print("Bitarray: ", bin(bytes)
+    print("Bitarray: ", bin(bytes))
     voltage = (summ / 0b111111111111) * MCP3202_VDD
     print("Voltage: " + str(voltage))
     sleep(0.05)
